650c8981952437cd2fa992cdb11cf4ece6bf23af
FakePickle <harsh22200@iiitd.ac.in>
2025-05-15 16:05:27 +0300

    refactor: removed unused imports

--- backend/flashfeed-api/app/main.py
+++ backend/flashfeed-api/app/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-from .database import Base, engine
-from .auth import router as auth_router
-from .users import router as user_router
 from .news import router as news_router
 
 Base.metadata.create_all(bind=engine)
